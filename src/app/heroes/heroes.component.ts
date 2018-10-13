350322b686163fd9b1205caa264a7b58291bfa5e
Olu Soboyede <dolapo@yinkos.com>
2018-10-13 23:34:37 +0100

    Routing
    Added Angular router to navigate among different components
    Turned AppComponent into a navigation shell with <a> links and a
    <router-outlet>
    Configured the router in an AppRoutingModule
    Defined simple route, a redirect route, and a parameterized route
    Used the routerLink directive in anchor elements
    Refactored a tightly-coupled master/detail view into a routed detail
    view
    Used router link parameters to navigate to the detail view of a
    user-selected hero
    Shared the HeroService among multiple components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,9 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
 
   constructor(private heroService: HeroService) { }
 
@@ -25,4 +18,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
